041b2fefb8fa2dd3aa487a31fd4d42dc325f9356
jimmyli <jyl702@gmail.com>
2011-11-19 17:25:06 -0500

    First commit.

---
+++ tab_sniper.js
@@ -0,0 +1,8 @@
+//1) Figure out how to select tabs
+//2) Figure out how to kill tabs
+//3) Figure out how to set up user UI, where user can input stuff
+//4) Let user's input determine which tabs to kill
+
+// chrome.tabs.getAllInWindow(null, function() {
+// 	console.log("worked!");
+// })
